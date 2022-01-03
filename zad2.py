c55268ce1cca21512639d521c0adcc002342c59a
astaciwa <01169353@pw.edu.pl>
2022-01-03 10:58:39 +0100

    zad 1 i 2

--- zad2.py
+++ zad2.py
@@ -2,6 +2,7 @@ import numpy as np
 
 def Funkcja(N):
     assert isinstance(N, int), "Podany parametr nie jest intem !"
+    assert N >= 1, "Parametr musi byc liczbą naturalną !"
     tab = np.arange(1, N+1)
     wynik = (1+1/tab)**tab
     roznica = np.e-wynik
@@ -10,6 +11,9 @@ def Funkcja(N):
 
 if __name__ == '__main__':
     try:
-        print(Funkcja(15))
+        wynikiObliczen = Funkcja(15)[0]
+        roznica = Funkcja(15)[1]
+        print(f"Wyniki obliczeń: \n {wynikiObliczen} \n\n Różnica poszczególnych wyników z liczbą Eulera: \n {roznica}")
+
     except(AssertionError) as err:
         print("Blad: ", err)
